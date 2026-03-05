4ae4c285376d0d8caa3fb3da932a6fd330c7276d
lureno hore <bulavaxiangreen@gmail.com>
2026-03-05 15:29:34 +0100

    fix railway start script

--- src/Server.js
+++ src/Server.js
@@ -9,10 +9,14 @@ import attendanceRoutes from "./routes/attendance.js";
 
 dotenv.config();
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}));
 app.use(express.json());
 
 
