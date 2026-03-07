cfc30dce60d0bef8f6c1da6ca266c3ed1a88f71b
lureno hore <bulavaxiangreen@gmail.com>
2026-03-07 01:45:52 +0100

    fix database connection

--- db.js
+++ db.js
@@ -1,24 +1,56 @@
-import mysql from "mysql2";
-import dotenv from "dotenv";
+const mysql = require("mysql2");
 
-dotenv.config();
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
-
-pool.getConnection((err, connection) => {
+connection.connect((err) => {
   if (err) {
     console.error("Database connection failed:", err);
   } else {
-    console.log("Connected to MySQL database");
-    connection.release();
+    console.log("Connected to Railway MySQL");
   }
 });
 
-export default pool.promise();
+module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mysql from "mysql2";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_Port,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+// pool.getConnection((err, connection) => {
+//   if (err) {
+//     console.error("Database connection failed:", err);
+//   } else {
+//     console.log("Connected to MySQL database");
+//     connection.release();
+//   }
+// });
+
+// export default pool.promise();
